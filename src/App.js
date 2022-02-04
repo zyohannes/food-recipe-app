1209f5641ce5e1a98f5e4072a72ab2b7ca09b93b
zyohannes <yohannestesfaye321@gmail.com>
2022-02-04 15:25:50 -0800

    Add form component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
+import "./App.css";
+import Form from "./components/Form";
 
 function App() {
+  const searchFood = async (e) => {
+    e.preventDefault();
+    console.log("searchFood");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-      
-              </header>
+      <header className="App-header">Tools</header>
+      <section>
+        <Form searchFood={searchFood} />
+      </section>
     </div>
   );
 }
